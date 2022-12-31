e56f22cae417b269488578288dfc577706ce7014
Kazuho OHTA <otkzh77@gmail.com>
2022-12-31 15:44:08 +0900

    autoplay設定

--- components/TopSlider.vue
+++ components/TopSlider.vue
@@ -1,56 +1,27 @@
 <template>
-  <swiper @swiper="onSwiper" @slideChange="onSlideChange" :options="swiperOptions">
+
+  <swiper :slidesPerView="1" :spaceBetween="30" :loop="true" :autoplay="{
+  delay: 8000,
+  disableOnInteraction: false,
+}" :loopAdditionalSlides="1" :modules="modules">
     <swiper-slide v-for="item in items" :key="item.id">
       <img :src="item.img" alt="alt" />
     </swiper-slide>
   </swiper>
 </template>
-<script>
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay } from "swiper";
+import "swiper/css";
+import 'swiper/css/effect-fade';
+const modules = [Autoplay];
 
-// Import Swiper styles
-import 'swiper/css';
-
-export default {
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  setup() {
-    const onSwiper = (swiper) => {
-      console.log(swiper);
-    };
-    const onSlideChange = () => {
-      console.log('slide change');
-    };
-    const items = [
-      { img: '/images/event230115seniorIdeathon.jpg' },
-      { img: '/images/project/project_shindan.jpg' },
-      { img: '/images/project/project_table.jpg' },
-      { img: '/images/project/project_kyouzai.jpg' },
-    ]
-    const swiperOptions = {
-      speed: 2000,
-      autoplay: {
-        delay: 8000,
-        disableOnInteraction: false,
-      },
-      loop: true,
-      loopAdditionalSlides: 1,
-      watchSlidesProgress: true,
-      nextButton: true,
-      prevButton: true,
-      effect: 'fade',
-    }
-    return {
-      onSwiper,
-      onSlideChange,
-      items,
-      swiperOptions,
-    };
-  },
-};
+const items = [
+  { img: '/images/event230115seniorIdeathon.jpg' },
+  { img: '/images/project/project_shindan.jpg' },
+  { img: '/images/project/project_table.jpg' },
+  { img: '/images/project/project_kyouzai.jpg' },
+]
 </script>
 
 <style lang="scss" scoped>
